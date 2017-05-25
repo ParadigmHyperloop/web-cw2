3ae4c90129d985a600bf8660c779a1976ed3c412
DEADP00l <DEADP00l@paradigmhyperloop.com>
2017-05-24 23:51:28 -0700

    Fix POST requests to ODS for all commands

--- src/app.js
+++ src/app.js
@@ -1,18 +1,20 @@
-var powerOn = document.getElementById("poweron");
-var lpFill = document.getElementById("lpfill");
-var hpFill = document.getElementById("hpfill");
-var armPod = document.getElementById("armpod");
-var shutDown = document.getElementById("shutdown");
-var ventState = document.getElementById("ventstate");
-var emergencyStop = document.getElementById("emergencystop");
-var telemetryData = document.getElementById("telemetrydata");
-var flightProfile = document.getElementById("flightprofile");
+// var powerOn = document.getElementById("poweron");
+// var lpFill = document.getElementById("lpfill");
+// var hpFill = document.getElementById("hpfill");
+// var armPod = document.getElementById("armpod");
+// var shutDown = document.getElementById("shutdown");
+// var ventState = document.getElementById("ventstate");
+// var emergencyStop = document.getElementById("emergencystop");
+// var telemetryData = document.getElementById("telemetrydata");
+// var flightProfile = document.getElementById("flightprofile");
 
 var CONFIG = {
   host: "127.0.0.1",
   port: 7777,
   proto: "http"
 }
+
+var GRAFANA_URL = "http://localhost:3000"
 
 STATES = {
   0 : { name: 'POST', color: 'default' },
@@ -35,11 +37,24 @@ function endpoint(path) {
   return CONFIG.proto + "://" + CONFIG.host + ":" + CONFIG.port.toString() + "/" + path;
 }
 
-// Onclick functions for buttons on the NavBar.
-function lpvent() {
+(function ($) {
+  function send_command(cmd) {
+    console.log("Sending Command: " + cmd.toString())
+    name = cmd[0]
+    args = []
+
+    if (cmd.length > 1) {
+      args = cmd.slice(1)
+    }
+
     $.ajax({
-        url: endpoint("command/lp_vent"),
-        type: 'POST',
+        url: endpoint("commands/" + name),
+        method: 'POST',
+        data: JSON.stringify({
+          args: args
+        }),
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
         success: function(response) {
             console.log(response);
         },
@@ -47,216 +62,175 @@ function lpvent() {
             console.log(error);
         }
     });
-    console.log('Current pod state = LPVENT.')
-}
+  }
 
-function hpfill() {
-    $.ajax({
-        url: endpoint("command/hp_fill"),
-        type: 'POST',
-        success: function(response) {
-            console.log(response);
-        },
-        error: function(error) {
-            console.log(error);
-        }
-    });
-    console.log('Current pod state = HP FILL.')
-}
+  $(function () {
+    // Onclick functions for buttons on the NavBar.
+    $('#lpVentBtn').on('click', function () {
+      send_command(["vent"])
+    })
 
-function armpod() {
-    $.ajax({
-        url: endpoint("command/arm"),
-        type: 'POST',
-        success: function(response) {
-            console.log(response);
-        },
-        error: function(error) {
-            console.log(error);
-        }
-    });
-    console.log('Arming the pod.')
-}
+    $('#hpFillBtn').on('click', function () {
+      send_command(["fill", "hp"])
+    })
 
-// Shutdown command needs implementation and big red button. 
-function shutdown() {
-    $.ajax({
-        url: endpoint("command/shutdown"),
-        type: 'POST',
-        success: function(response) {
-            console.log(response);
-        },
-        error: function(error) {
-            console.log(error);
-        }
-    });
-    console.log('Shutting the pod down.');
-}
+    $('#armPodBtn').on('click', function () {
+      send_command(["arm"])
+    })
 
-// Calibration command goes here (calibrate) zeros out IMU and nav data.
-function calibrate() {
-    $.ajax({
-        url: endpoint("command/calibrate"),
-        type: 'POST',
-        success: function(response) {
-            console.log(response);
-        },
-        error: function(error) {
-            console.log(error);
-        }
-    });
-    console.log('Calibrating Pod. Zeroing out IMU and nav data.')
-}
+    // Shutdown command needs implementation and big red button.
+    $('#shutdownBtn').on('click', function () {
+      send_command(["shutdown"])
+    })
 
-function emergencystop() {
-    $.ajax({
-        url: endpoint("command/e"),
-        type: 'POST',
-        success: function(response) {
-            console.log(response);
-        },
-        error: function(error) {
-            console.log(error);
-        }
-    });
-    console.log('Bringing the pod to an emergency stop.')
-};
+    // Calibration command goes here (calibrate) zeros out IMU and nav data.
+    $('#calibrateBtn').on('click', function () {
+      send_command(["calibrate"])
+    })
 
-function telemetrydata() {
-	console.log('Bringing the user to the telemetry data graphs.')
-}
-function flightprofile() {
-	console.log('Bringing the user to the flight profile data.')
-}
+    $('#emergencyBtn').on('click', function () {
+      send_command(["emergency"])
+    })
 
-// Gets PodState JSON Blob and parses its data into variables.
-// Variables are accessed in the below functions to update the GUI.
-setInterval(updatePodState, 1000);
-var podState;
+    $('#telemetryDataBtn').on('click', function () {
+    	window.open(GRAFANA_URL)
+    })
 
-function update_pod_state(num) {
-  console.log("state number: " + num.toString())
+    $('#flightProfileBtn').on('click', function () {
+    	console.log('Bringing the user to the flight profile data.')
+    })
+  })
 
-  var state = STATES[num]
-  console.log("state: " + state.name + " " + state.color)
-  $('#podState').text(state.name).addClass("badge-" + state.color)
+  // Gets PodState JSON Blob and parses its data into variables.
+  // Variables are accessed in the below functions to update the GUI.
+  setInterval(updatePodState, 1000);
+  var podState;
 
-  colors = ['primary', 'default', 'danger', 'warning', 'success', 'info']
-  for (var i in colors) {
-    if (colors[i] != state.color) {
-      $('#podState').removeClass("badge-" + colors[i])
-      console.log("removed badge-" + colors[i])
+  function update_pod_state(num) {
+    if (num === undefined) {
+      return
+    }
+    console.log("state number: " + num.toString())
 
+    var state = STATES[num]
+    console.log("state: " + state.name + " " + state.color)
+    $('#podState').text(state.name).addClass("badge-" + state.color)
+
+    colors = ['primary', 'default', 'danger', 'warning', 'success', 'info']
+    for (var i in colors) {
+      if (colors[i] != state.color) {
+        $('#podState').removeClass("badge-" + colors[i])
+        console.log("removed badge-" + colors[i])
+
+      }
     }
   }
-}
 
-function update_badge_valve_nc(id, value) {
-    if (!valid_valve_value(value)) {
+  function update_badge_valve_nc(id, value) {
+      if (!valid_valve_value(value)) {
+        console.log("Valve: " + id + " has invalid values")
+        return;
+      }
+
+      if (value == 1) {
+          $(id).text('Open').addClass('badge-danger').removeClass('badge-success');
+          console.log("Valve " + id + " Open")
+      } else {
+          $(id).text('Closed').addClass('badge-success').removeClass('badge-danger');
+          console.log("Valve " + id + " Closed")
+      }
+  }
+
+  function update_badge_valve_no(id, value) {
+    update_badge_valve_nc(id, !value);
+  }
+
+  function update_badge_valve_brake(id, value_rel, value_eng) {
+    if (!valid_valve_value(value_rel) || !valid_valve_value(value_eng)) {
       console.log("Valve: " + id + " has invalid values")
       return;
     }
 
-    if (value == 1) {
-        $(id).text('Open').addClass('badge-danger').removeClass('badge-success');
-        console.log("Valve " + id + " Open")
+    if (value_eng == 0 && value_rel == 0) {
+        $(id).text('Closed').addClass('badge-success').removeClass('badge-warning').removeClass('badge-danger').removeClass('badge-primary');
+        console.log("Brake " + id + " is Closed")
+    } else if (value_eng == 1 && value_rel == 0) {
+        $(id).text('Engaged').addClass('badge-warning').removeClass('badge-success').removeClass('badge-danger').removeClass('badge-primary');
+        console.log("Brake " + id + " is Engaged")
+    } else if (value_eng == 0 && value_rel == 1) {
+        $(id).text('Released').addClass('badge-primary').removeClass('badge-success').removeClass('badge-danger').removeClass('badge-warning');
+        console.log("Brake " + id + " is Released")
     } else {
-        $(id).text('Closed').addClass('badge-success').removeClass('badge-danger');
-        console.log("Valve " + id + " Closed")
+        $(id).text('Error').addClass('badge-danger').removeClass('badge-success').removeClass('badge-warning');
+        console.log("Brake " + id + " is Error: eng:" + value_eng.toString() + " rel:" + value_rel.toString())
     }
-}
-
-function update_badge_valve_no(id, value) {
-  update_badge_valve_nc(id, !value);
-}
-
-function update_badge_valve_brake(id, value_rel, value_eng) {
-  if (!valid_valve_value(value_rel) || !valid_valve_value(value_eng)) {
-    console.log("Valve: " + id + " has invalid values")
-    return;
   }
 
-  if (value_eng == 0 && value_rel == 0) {
-      $(id).text('Closed').addClass('badge-success').removeClass('badge-warning').removeClass('badge-danger').removeClass('badge-primary');
-      console.log("Brake " + id + " is Closed")
-  } else if (value_eng == 1 && value_rel == 0) {
-      $(id).text('Engaged').addClass('badge-warning').removeClass('badge-success').removeClass('badge-danger').removeClass('badge-primary');
-      console.log("Brake " + id + " is Engaged")
-  } else if (value_eng == 0 && value_rel == 1) {
-      $(id).text('Released').addClass('badge-primary').removeClass('badge-success').removeClass('badge-danger').removeClass('badge-warning');
-      console.log("Brake " + id + " is Released")
-  } else {
-      $(id).text('Error').addClass('badge-danger').removeClass('badge-success').removeClass('badge-warning');
-      console.log("Brake " + id + " is Error: eng:" + value_eng.toString() + " rel:" + value_rel.toString())
+  function valid_valve_value(value) {
+    return (value === 1 || value  === 0)
   }
-}
 
-function valid_valve_value(value) {
-  return (value === 1 || value  === 0)
-}
+  function updatePodState() {
+      $.ajax({
+          url: endpoint("state"),
+          type: 'GET',
+          success: function(podState) {
+              console.log(podState);
 
-function updatePodState() {
-    $.ajax({
-        url: endpoint("state"),
-        type: 'GET',
-        success: function(response) {
-            console.log(response);
-            podState = JSON.parse(response);
-
-            var new_values = {
-              nc: {
-                '#solenoidSkateA1' : podState.SOL_SKATE_0,
-                '#solenoidSkateB1' : podState.SOL_SKATE_0,
-                '#solenoidSkateC1' : podState.SOL_SKATE_1,
-                '#solenoidSkateD1' : podState.SOL_SKATE_1,
-                '#solenoidSkateE1' : podState.SOL_SKATE_2,
-                '#solenoidSkateF1' : podState.SOL_SKATE_2,
-                '#highPressureFill' : podState.SOL_HPFIL,
-                '#solenoidBrake1-1' : podState.SOL_CLAMP_FIL_0,
-                '#solenoidBrake2-1' : podState.SOL_CLAMP_FIL_1
-              },
-              no: {
-                '#lowPressureVent' : podState.SOL_VENT
-              },
-              brake: {
-                '#solenoidBrake1-2' : [
-                  podState.SOL_CLAMP_REL_0,
-                  podState.SOL_CLAMP_ENG_0
-                ],
-                '#solenoidBrake2-2' : [
-                  podState.SOL_CLAMP_REL_1,
-                  podState.SOL_CLAMP_ENG_1
-                ]
+              var new_values = {
+                nc: {
+                  '#solenoidSkateA1' : podState.SOL_SKATE_0,
+                  '#solenoidSkateB1' : podState.SOL_SKATE_0,
+                  '#solenoidSkateC1' : podState.SOL_SKATE_1,
+                  '#solenoidSkateD1' : podState.SOL_SKATE_1,
+                  '#solenoidSkateE1' : podState.SOL_SKATE_2,
+                  '#solenoidSkateF1' : podState.SOL_SKATE_2,
+                  '#highPressureFill' : podState.SOL_HPFIL,
+                  '#solenoidBrake1-1' : podState.SOL_CLAMP_FIL_0,
+                  '#solenoidBrake2-1' : podState.SOL_CLAMP_FIL_1
+                },
+                no: {
+                  '#lowPressureVent' : podState.SOL_VENT
+                },
+                brake: {
+                  '#solenoidBrake1-2' : [
+                    podState.SOL_CLAMP_REL_0,
+                    podState.SOL_CLAMP_ENG_0
+                  ],
+                  '#solenoidBrake2-2' : [
+                    podState.SOL_CLAMP_REL_1,
+                    podState.SOL_CLAMP_ENG_1
+                  ]
+                }
               }
-            }
 
-            for (var key in new_values['nc']) {
-              update_badge_valve_nc(key, new_values['nc'][key]);
-            }
+              for (var key in new_values['nc']) {
+                update_badge_valve_nc(key, new_values['nc'][key]);
+              }
 
-            for (var key in new_values['no']) {
-              update_badge_valve_no(key, new_values['no'][key]);
-            }
+              for (var key in new_values['no']) {
+                update_badge_valve_no(key, new_values['no'][key]);
+              }
 
-            for (var key in new_values['brake']) {
-              update_badge_valve_brake(key, new_values['brake'][key][0], new_values['brake'][key][1]);
-            }
+              for (var key in new_values['brake']) {
+                update_badge_valve_brake(key, new_values['brake'][key][0], new_values['brake'][key][1]);
+              }
 
-            var pressureTransducer = podState.hp_pressure
-            $('#psiTransducer').text(pressureTransducer);
+              var pressureTransducer = podState.hp_pressure
+              $('#psiTransducer').text(pressureTransducer);
 
-            var stripeCount = podState.stripe_count
-            $('#stripeCount').text(stripeCount);
+              var stripeCount = podState.stripe_count
+              $('#stripeCount').text(stripeCount);
 
 
-            var approxDistance = podState.position_x
-            $('#approxDist').text(approxDistance);
+              var approxDistance = podState.position_x
+              $('#approxDist').text(approxDistance);
 
-            update_pod_state(podState.state)
-        },
-        error: function(error) {
-            console.log(error);
-        }
-    });
-}
-
+              update_pod_state(podState.state)
+          },
+          error: function(error) {
+              console.log(error);
+          }
+      });
+  }
+})(jQuery)
